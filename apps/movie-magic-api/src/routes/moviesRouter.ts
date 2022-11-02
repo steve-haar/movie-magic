d852bcc41c3c0ffbe6ba97505845c37c6b3444fd
Naresh Bhatia <me@nareshbhatia.dev>
2022-11-01 22:53:19 -0400

    Added API to get a movie by id

--- apps/movie-magic-api/src/routes/moviesRouter.ts
+++ apps/movie-magic-api/src/routes/moviesRouter.ts
@@ -7,3 +7,13 @@ export const moviesRouter = express.Router();
 moviesRouter.get('/', (_, res) => {
   res.send(movies);
 });
+
+/** get movie with specified id */
+moviesRouter.get('/:id', (req, res) => {
+  const movie = movies.find((movie) => movie.id === req.params.id);
+  if (movie) {
+    res.send(movie);
+  } else {
+    res.status(404).end();
+  }
+});
